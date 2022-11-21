7221d36d52d8ebe82399889aec028a7d7c67cee4
vusalaxndzde <vusal.axndzde@gmail.com>
2022-11-21 23:00:54 +0400

    changed

--- ResumeDesktopApp/src/main/java/com/company/resume/main/MainUser.java
+++ ResumeDesktopApp/src/main/java/com/company/resume/main/MainUser.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package main;
+package com.company.resume.main;
 
 import com.mycompany.dao.inter.CountryDaoInter;
 import com.mycompany.dao.inter.UserDaoInter;
