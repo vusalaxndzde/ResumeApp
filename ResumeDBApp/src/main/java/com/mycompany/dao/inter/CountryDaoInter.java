44a2c4d10a8330cf1dcf0456082fadb7e9935045
vusalaxndzde <vusal.axndzde@gmail.com>
2022-11-23 01:09:26 +0400

    updated countryDaoInter

--- ResumeDBApp/src/main/java/com/mycompany/dao/inter/CountryDaoInter.java
+++ ResumeDBApp/src/main/java/com/mycompany/dao/inter/CountryDaoInter.java
@@ -6,5 +6,13 @@ import java.util.List;
 public interface CountryDaoInter {
 
     public List<Country> getAllCountry();
+    
+    public Country getCountryById(int id);
+    
+    public boolean updateCountry(Country c);
+    
+    public boolean addCountry(Country c);
+    
+    public boolean removeCountry(int id);
 
 }
