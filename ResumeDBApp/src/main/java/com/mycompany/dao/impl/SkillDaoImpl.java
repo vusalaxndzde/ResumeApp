c2377d67081ed23ec764e80f0d0c08238fd01dc9
vusalaxndzde <vusal.axndzde@gmail.com>
2022-11-22 18:17:26 +0400

    updated skillDao

--- ResumeDBApp/src/main/java/com/mycompany/dao/impl/SkillDaoImpl.java
+++ ResumeDBApp/src/main/java/com/mycompany/dao/impl/SkillDaoImpl.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.dao.impl;
 
 import com.mycompany.dao.inter.AbstractDAO;
@@ -15,10 +11,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Asus
- */
 public class SkillDaoImpl extends AbstractDAO implements SkillDaoInter {
 
     public Skill getSkill(ResultSet rs) throws Exception {
