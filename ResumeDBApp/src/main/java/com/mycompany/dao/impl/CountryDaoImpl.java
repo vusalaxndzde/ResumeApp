7fa847081e603c29469c33dab871095869f36e8b
vusalaxndzde <vusal.axndzde@gmail.com>
2022-11-22 19:31:15 +0400

    changed

--- ResumeDBApp/src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
+++ ResumeDBApp/src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.dao.impl;
 
 import com.mycompany.dao.inter.AbstractDAO;
@@ -13,10 +9,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Asus
- */
 public class CountryDaoImpl extends AbstractDAO implements CountryDaoInter {
 
     public Country getCountry(ResultSet rs) throws Exception {
--- ResumeDBApp/src/main/java/com/mycompany/dao/impl/EmploymentHistoryDaoImpl.java
+++ ResumeDBApp/src/main/java/com/mycompany/dao/impl/EmploymentHistoryDaoImpl.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.dao.impl;
 
 import com.mycompany.dao.inter.AbstractDAO;
@@ -15,10 +11,6 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Asus
- */
 public class EmploymentHistoryDaoImpl extends AbstractDAO implements EmploymentHistoryDaoInter {
     
     public EmploymentHistory getEmploymentHistory(ResultSet rs) throws Exception {
