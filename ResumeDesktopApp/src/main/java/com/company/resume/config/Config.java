fe9ee73e67470f14d7d6cfd85cdd65fb18252069
vusalaxndzde <vusal.axndzde@gmail.com>
2022-11-23 23:33:50 +0400

    changed

--- ResumeDesktopApp/src/main/java/com/company/resume/config/Config.java
+++ ResumeDesktopApp/src/main/java/com/company/resume/config/Config.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.company.resume.config;
 
 import com.mycompany.entity.User;
 
-/**
- *
- * @author Asus
- */
 public class Config {
     
     public static User loggedInUser;
