a2738a00b1e2147e3335ce1cf1b95b581d789673
vusalaxndzde <vusal.axndzde@gmail.com>
2022-12-05 02:33:20 +0400

    changed

--- ResumeDBApp/src/main/java/com/mycompany/dao/inter/AbstractDAO.java
+++ ResumeDBApp/src/main/java/com/mycompany/dao/inter/AbstractDAO.java
@@ -2,13 +2,12 @@ package com.mycompany.dao.inter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 
 public abstract class AbstractDAO {
     
-    public Connection connect() throws SQLException {
-        //Class.forName("com.mysql.jdbc.Driver");
+    public Connection connect() throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/resume";
         String username = "root";
         String password = "12345";
