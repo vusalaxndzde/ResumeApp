135ebdd01cc9e4751b285fef63dcb91f96b0f178
vusalaxndzde <vusal.axndzde@gmail.com>
2022-12-15 17:18:23 +0400

    login controller

--- ResumeWebApp/src/main/java/com/company/resumewebapp/LoginController.java
+++ ResumeWebApp/src/main/java/com/company/resumewebapp/LoginController.java
@@ -1,20 +1,29 @@
 package com.company.resumewebapp;
 
+import com.mycompany.dao.inter.UserDaoInter;
+import com.mycompany.entity.User;
+import com.mycompany.main.Context;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LoginController", value = "/LoginController")
 public class LoginController extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
+    UserDaoInter userDao = Context.instanceUserDao();
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        List<User> users = userDao.filter(username, password, null);
+        if (!(users.size() == 0)) {
+            response.sendRedirect("users.jsp");
+        } else {
+            response.sendError(404);
+        }
     }
 }
