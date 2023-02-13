c29c2e5958acf6bc8f639a634afd584a9ce9a662
vusalaxndzde <vusal.axndzde@gmail.com>
2023-02-14 00:33:40 +0400

    initial commit

---
+++ ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/ResumeAppJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAppJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
