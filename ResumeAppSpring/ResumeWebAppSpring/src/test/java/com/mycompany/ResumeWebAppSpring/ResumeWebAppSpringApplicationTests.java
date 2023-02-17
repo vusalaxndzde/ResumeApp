d1539e2c76361425706b412ae90c620d984e1d3d
vusalaxndzde <vusal.axndzde@gmail.com>
2023-02-18 01:38:31 +0400

    spring web app

---
+++ ResumeAppSpring/ResumeWebAppSpring/src/test/java/com/mycompany/ResumeWebAppSpring/ResumeWebAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.ResumeWebAppSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeWebAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
