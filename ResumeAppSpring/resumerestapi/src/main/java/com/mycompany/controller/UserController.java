f15a8bde5ca37cb5c17ccbae95e3152f7791f3c5
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-07 01:47:38 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/controller/UserController.java
+++ ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/controller/UserController.java
@@ -1,15 +1,32 @@
 package com.mycompany.controller;
 
+import com.mycompany.dao.impl.UserRepositoryCustom;
+import com.mycompany.dto.UserDTO;
+import com.mycompany.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class UserController {
 
+    @Autowired
+    @Qualifier("userDao1")
+    private UserRepositoryCustom userRepo;
+
     @GetMapping("users")
-    public ResponseEntity<String> getUsers() {
-        return ResponseEntity.ok().body("Salam");
+    public ResponseEntity<List> getUsers() {
+        List<UserDTO> userDTOS = new ArrayList<>();
+        List<User> users = userRepo.getAll();
+        for (User u : users) {
+            userDTOS.add(new UserDTO(u));
+        }
+        return ResponseEntity.ok().body(userDTOS);
     }
 
 }
