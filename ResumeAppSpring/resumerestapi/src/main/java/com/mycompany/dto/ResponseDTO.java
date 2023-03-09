616d02814d7a9efe8526bb50d6fa60c8e0fce10e
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-09 22:06:09 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/dto/ResponseDTO.java
+++ ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/dto/ResponseDTO.java
@@ -1,4 +1,66 @@
 package com.mycompany.dto;
 
 public class ResponseDTO {
+
+    private String successMsg;
+    private Integer errorCode;
+    private String errorMsg;
+    private Object object;
+
+    private ResponseDTO() {
+
+    }
+
+    public static ResponseDTO of(Object object) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setObject(object);
+        return responseDTO;
+    }
+
+    public static ResponseDTO of(Object object, String successMsg) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setObject(object);
+        responseDTO.setSuccessMsg(successMsg);
+        return responseDTO;
+    }
+
+    public static ResponseDTO of(Integer errorCode, String errorMsg) {
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setErrorCode(errorCode);
+        responseDTO.setErrorMsg(errorMsg);
+        return responseDTO;
+    }
+
+    public String getSuccessMsg() {
+        return successMsg;
+    }
+
+    public void setSuccessMsg(String successMsg) {
+        this.successMsg = successMsg;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(Integer errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
 }
