d366729b9a0333ce45c561dc09c15cf0b7159a42
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-09 20:21:18 +0400

    changes

---
+++ ResumeAppSpring/resumerestclient/src/main/java/com/mycompany/ResumerestclientApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumerestclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumerestclientApplication.class, args);
+	}
+
+}
