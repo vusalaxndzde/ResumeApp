648f76cff5e63209fc0bb1fe023f1d476cdabd03
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-16 00:40:52 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/security/SecurityConfig.java
+++ ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/security/SecurityConfig.java
@@ -62,8 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Primary
-    //Making this primary to avoid any accidental duplication with another token service instance of the same name
+    @Primary //Making this primary to avoid any accidental duplication with another token service instance of the same name
     public DefaultTokenServices tokenServices() {
         DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
         defaultTokenServices.setTokenStore(tokenStore());
@@ -85,6 +84,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable();
 
     }
-
 
 }
