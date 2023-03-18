9f5988f21a74867a5f40cac2fb489ba0f5c61d4f
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-19 01:48:10 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/aspect/ExceptionLogAdvice.java
+++ ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/aspect/ExceptionLogAdvice.java
@@ -1,10 +1,16 @@
 package com.mycompany.aspect;
 
+import com.mycompany.dto.ResponseDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionLogAdvice {
 
-    
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ResponseDTO> exceptionHandler(Exception ex) {
+        return ResponseEntity.internalServerError().body(ResponseDTO.of(500, ex.getMessage()));
+    }
 
 }
