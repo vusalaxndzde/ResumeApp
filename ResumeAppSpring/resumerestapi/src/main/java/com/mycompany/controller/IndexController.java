cc9e0d0459b93bc0cb7ad0ea12c614c975b70081
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-23 00:58:11 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/controller/IndexController.java
+++ ResumeAppSpring/resumerestapi/src/main/java/com/mycompany/controller/IndexController.java
@@ -3,8 +3,8 @@ package com.mycompany.controller;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -15,11 +15,10 @@ public class IndexController {
     }
 
     @GetMapping("/userinfo")
-    public ModelAndView userInfo(ModelAndView modelAndView, @AuthenticationPrincipal OAuth2User user) {
-        modelAndView.setViewName("userinfo");
-        modelAndView.addObject("name", user.getAttribute("name"));
-        modelAndView.addObject("email", user.getAttribute("email"));
-        return modelAndView;
+    public String userInfo(Model model, @AuthenticationPrincipal OAuth2User user) {
+        model.addAttribute("name", user.getAttribute("name"));
+        model.addAttribute("email", user.getAttribute("email"));
+        return "userinfo";
     }
 
 }
