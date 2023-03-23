0dc550fb251efc6da88378ecb8617a4356b00b5f
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-24 02:08:12 +0400

    testing

--- ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/service/UserServiceTest.java
+++ ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/service/UserServiceTest.java
@@ -4,10 +4,15 @@ import com.mycompany.dao.impl.UserRepositoryCustom;
 import com.mycompany.entity.User;
 import com.mycompany.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceTest {
@@ -18,11 +23,28 @@ public class UserServiceTest {
     @InjectMocks
     private UserServiceImpl userService;
 
+    @BeforeAll
+    public static void setUp() {
+        System.out.println("setup called");
+    }
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Before called");
+        MockitoAnnotations.openMocks(this);
+
+        List<User> users = new ArrayList<>();
+        User user = new User(null, "test@gmail.com", "test");
+
+        users.add(user);
+
+        Mockito.when(userRepo.getAll()).thenReturn(users);
+    }
+
     @Test
     public void getAllTest() {
         List<User> users = userService.getAll();
-
-        Assertions.assertEquals(1, users.size(), "1 usser");
+        Assertions.assertEquals(1, users.size(), "users size must be 1");
     }
 
 }
