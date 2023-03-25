cde854db4f4cdf38965be61144eeb2269a88a433
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-26 01:16:57 +0400

    changes

--- ResumeAppSpring/resumerestapi/src/test/java/com/mycompany/controller/UserControllerTest.java
+++ ResumeAppSpring/resumerestapi/src/test/java/com/mycompany/controller/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.mycompany.controller;
 import com.mycompany.ResumerestapiApplication;
 import com.mycompany.dto.ResponseDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,9 +20,26 @@ public class UserControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @DisplayName("Get Users")
     @Test
     public void getUsersTest() {
         ResponseEntity<ResponseDTO> response = restTemplate.getForEntity("/users", ResponseDTO.class);
+        Assertions.assertSame(response.getStatusCode(), HttpStatus.OK);
+        System.out.println(response.getBody());
+    }
+
+    @DisplayName("Filter")
+    @Test
+    public void filterTest() {
+        ResponseEntity<ResponseDTO> response = restTemplate.getForEntity("/users/filter?name=vusal&surname=axundzade", ResponseDTO.class);
+        Assertions.assertSame(response.getStatusCode(), HttpStatus.OK);
+        System.out.println(response.getBody());
+    }
+
+    @DisplayName("Get User")
+    @Test
+    public void getUserTest() {
+        ResponseEntity<ResponseDTO> response = restTemplate.getForEntity("/users/18", ResponseDTO.class);
         Assertions.assertSame(response.getStatusCode(), HttpStatus.OK);
         System.out.println(response.getBody());
     }
