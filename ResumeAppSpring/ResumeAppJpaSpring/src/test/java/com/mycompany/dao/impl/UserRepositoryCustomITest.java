52d5584b706a3e78248a611e635a6fc0c644d105
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-25 23:33:10 +0400

    changes

--- ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
+++ ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
@@ -2,17 +2,20 @@ package com.mycompany.dao.impl;
 
 import com.mycompany.entity.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 //@ExtendWith(SpringExtension.class)
+//@DataJpaTest
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryCustomITest {
@@ -21,10 +24,11 @@ public class UserRepositoryCustomITest {
     @Qualifier(value = "userDao1")
     private UserRepositoryCustom userRepo;
 
+    @DisplayName("Get All")
     @Test
     public void getAll() {
         List<User> users = userRepo.getAll();
-
+        System.out.println(users);
         Assertions.assertEquals(4, users.size(), "users size must be 4");
     }
 
