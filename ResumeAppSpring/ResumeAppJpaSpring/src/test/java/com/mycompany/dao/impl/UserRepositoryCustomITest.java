329fb46f6443ad19a4cd608e70c0b2762e11cad5
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-25 23:19:28 +0400

    changes

--- ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
+++ ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
@@ -1,4 +1,31 @@
 package com.mycompany.dao.impl;
 
+import com.mycompany.entity.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryCustomITest {
+
+    @Autowired
+    @Qualifier(value = "userDao1")
+    private UserRepositoryCustom userRepo;
+
+    @Test
+    public void getAll() {
+        List<User> users = userRepo.getAll();
+
+        Assertions.assertEquals(4, users.size(), "users size must be 4");
+    }
+
 }
