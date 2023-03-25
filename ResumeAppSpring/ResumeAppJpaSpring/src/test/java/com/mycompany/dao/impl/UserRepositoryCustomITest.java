9f7ae876a739fd8dab66c46a4adf96fda8814a83
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-26 00:54:25 +0400

    changes

--- ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
+++ ResumeAppSpring/ResumeAppJpaSpring/src/test/java/com/mycompany/dao/impl/UserRepositoryCustomITest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
--- ResumeAppSpring/resumerestapi/src/test/java/com/mycompany/controller/UserControllerTest.java
+++ ResumeAppSpring/resumerestapi/src/test/java/com/mycompany/controller/UserControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(
         classes = ResumerestapiApplication.class,
