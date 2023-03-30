fcd3527e24efe00e9e6a3596b67bd650b3daa891
vusalaxndzde <vusal.axndzde@gmail.com>
2023-03-30 23:10:35 +0400

    changes

--- ResumeAppSpring/ResumeWebAppSpring/src/main/java/com/mycompany/controller/IndexController.java
+++ ResumeAppSpring/ResumeWebAppSpring/src/main/java/com/mycompany/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.mycompany.controller;
 
 import com.mycompany.dao.impl.UserRepositoryCustom;
+import com.mycompany.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +18,9 @@ public class IndexController {
     private UserRepositoryCustom userRepo;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index() {
+    public ModelAndView index(Authentication authentication) {
+        String email = authentication.getName();
+        User loggedInUser = userRepo.getUserByEmail(email);
         ModelAndView model = new ModelAndView("index");
         return model;
     }
